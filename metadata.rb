c61fba129be417a9c0a22caf1f8025839b091c62
Joseph Chen <joe1chen@hotmail.com>
2014-06-26 17:44:05 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nutch2"
 maintainer        "Timo Schmidt, Joe Chen"
 description       "Installs Nutch 2 on cloudera 4"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0.1"
+version           "1.0.1"
 
 depends "cloudera"
 depends "ant"
